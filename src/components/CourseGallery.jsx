913918844e17f4a105bc2d19a70f2439c3f28de2
Tareq Sefati <tareq.sefati@gmail.com>
2024-10-04 16:00:07 +0600

    Implement course gallery UI section.

--- src/components/CourseGallery.jsx
+++ src/components/CourseGallery.jsx
@@ -18,21 +18,21 @@ export default function CourseGallery() {
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
+									src="/images/java.jpg"
 								/>
 							</div>
 							<div className="w-1/2 p-1 md:p-2">
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+									src="/images/javafx-1.png"
 								/>
 							</div>
 							<div className="w-full p-1 md:p-2">
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+									src="/images/springboot.jpg"
 								/>
 							</div>
 						</div>
@@ -41,21 +41,21 @@ export default function CourseGallery() {
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+									src="/images/nextjs.webp"
 								/>
 							</div>
 							<div className="w-1/2 p-1 md:p-2">
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+									src="/images/reactjs.png"
 								/>
 							</div>
 							<div className="w-1/2 p-1 md:p-2">
 								<img
 									alt="gallery"
 									className="block h-full w-full rounded-lg object-cover object-center"
-									src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+									src="/images/mongodb.jpg"
 								/>
 							</div>
 						</div>
